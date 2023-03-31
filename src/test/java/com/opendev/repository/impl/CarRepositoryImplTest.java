216bc167b65f1bff2127219d9f6528d57bd8a7e3
avalos.m <mora_m_avalos@hotmail.com>
2023-03-31 18:05:48 -0300

    test calcular costo pausa

--- src/test/java/com/opendev/repository/impl/CarRepositoryImplTest.java
+++ src/test/java/com/opendev/repository/impl/CarRepositoryImplTest.java
@@ -12,13 +12,14 @@ import org.junit.Test;
 import com.opendev.entity.Car;
 import com.opendev.entity.Model;
 import com.opendev.entity.Optional;
+import com.opendev.repository.CarRepository;
 import com.opendev.repository.impl.CarRepositoryImpl;
 
 public class CarRepositoryImplTest {
 
 	@Test
 	public void queFuncioneCorrectamenteElMetodoSave() {
-		CarRepositoryImpl cri = new CarRepositoryImpl();
+		CarRepository cri = new CarRepositoryImpl();
 
 		Model modelo = new Model(1, "Sedan", 230000.0);
 
@@ -35,7 +36,7 @@ public class CarRepositoryImplTest {
 
 	@Test
 	public void queSePuedaGuardarUnAutoYDespuesBorrarlo() {
-		CarRepositoryImpl cri = new CarRepositoryImpl();
+		CarRepository cri = new CarRepositoryImpl();
 		// setup
 		Model model = new Model(2, "Familiar", 270000.0);
 		Optional optional1 = new Optional(2, "AA", "Aire acondicionado", 20000.0);
@@ -53,13 +54,26 @@ public class CarRepositoryImplTest {
 	}
 
 	@Test
-	public void queFuncioneCorrectamenteStatsModel() {
-		//pausa
+	public void queSePuedaActualizarUnAutoDespuesDeCrearloYCalcularSuCosto() {
+		Integer idCar = 4;
+		Model model = new Model(3, "Coupé", 270000.0);
+		Optional opcional1 = new Optional(1, "TC", "Techo corredizo", 12000.0);
+		Optional opcional2 = new Optional(2, "AA", "Aire acondicionado", 20000.0);
+		Optional opcional3 = new Optional(3, "ABS", "Sistemas de frenos ABS", 14000.0);
+		Optional opcional4 = new Optional(4, "AB", "Airbag", 7000.0);
+		Set<Optional> opcionales = new HashSet<>();
+		opcionales.add(opcional1);
+		opcionales.add(opcional2);
+		opcionales.add(opcional3);
+		opcionales.add(opcional4);
+		
+		Car car = new Car(idCar, model, opcionales);
 
 	}
+
 	@Test
 	public void queFuncioneCorrectamenteStatsOptional() {
-		//pausa
+		// pausa
 
 	}
 
--- src/test/java/com/opendev/service/impl/CarServiceImplTest.java
+++ src/test/java/com/opendev/service/impl/CarServiceImplTest.java
@@ -21,7 +21,7 @@ import com.opendev.service.impl.CarServiceImpl;
 public class CarServiceImplTest {
 
 	public void queSePuedaCrearYBorrarUnAuto() {
-		
+
 		Integer id = 1;
 		Model model = new Model(1, "Sedán", 230000.0);
 		Optional opcional = new Optional(2, "AA", "Aire acondicionado", 20000.0);
@@ -43,29 +43,32 @@ public class CarServiceImplTest {
 
 	}
 
-//	@Test
-//	public void metodoCreateUpdateYDeleteCorrectoFuncionamiento() {
-//		CarService csi = new CarServiceImpl();
-//		Integer idModel = 21;
-//		Model model = new Model(idModel, "", 200000.00);
-//
-//		Optional opcional = new Optional();
-//		Optional opcional2 = new Optional();
-//
-//		Integer idOptional1 = opcional.getId();
-//		Integer idOptional2 = opcional2.getId();
-//
-//		Set<Integer> opcionales = new HashSet<Integer>();
-//		opcionales.add(idOptional1);
-//		opcionales.add(idOptional2);
-//
-//		// Car car = new Car();
-//		Integer newId = 550;
-////		car.setId(newId);
-//
-//		csi.create(model.getId(), opcionales);
-//		csi.update(newId, idModel, opcionales);
-//		assertTrue(csi.delete(newId));
-//	}
+	@Test
+	public void queSePuedaActualizarUnAutoDespuesDeCrearloYCalcularSuCosto() {
+		Integer idCar = 4;
+		Model model = new Model(3, "Coupé", 270000.0);
+		Optional opcional1 = new Optional(1, "TC", "Techo corredizo", 12000.0);
+		Optional opcional2 = new Optional(2, "AA", "Aire acondicionado", 20000.0);
+		Optional opcional3 = new Optional(3, "ABS", "Sistemas de frenos ABS", 14000.0);
+		Optional opcional4 = new Optional(4, "AB", "Airbag", 7000.0);
+		Set<Optional> opcionales = new HashSet<>();
+		opcionales.add(opcional1);
+		opcionales.add(opcional2);
+		opcionales.add(opcional3);
+		opcionales.add(opcional4);
+		
+		Set<Integer> idOpcionales = new HashSet<>();
+		idOpcionales.add(opcional1.getId());
+		idOpcionales.add(opcional2.getId());
+		idOpcionales.add(opcional3.getId());
+		idOpcionales.add(opcional4.getId());
+
+		Car car = new Car(idCar, model, opcionales);
+		
+		CarService cs = new CarServiceImpl();
+		cs.create(model.getId(), idOpcionales);
+		cs.update(2, 1, idOpcionales);
+
+	}
 
 }
