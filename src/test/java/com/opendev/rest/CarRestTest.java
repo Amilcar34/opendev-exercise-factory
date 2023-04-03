54d4582438a5912a721c9a7ea2a0593d0dd8aae3
avalos.m <mora_m_avalos@hotmail.com>
2023-04-03 18:07:55 -0300

    calcular costo

--- src/test/java/com/opendev/rest/CarRestTest.java
+++ src/test/java/com/opendev/rest/CarRestTest.java
@@ -1,6 +1,7 @@
 package com.opendev.rest;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -8,6 +9,7 @@ import java.util.Set;
 import org.junit.Test;
 
 import com.opendev.dto.CarDto;
+import com.opendev.entity.Model;
 import com.opendev.entity.Optional;
 
 public class CarRestTest {
@@ -17,13 +19,53 @@ public class CarRestTest {
 
 		CarRest cr = new CarRest();
 
-		int model = 2;
+		int modelId = 2;
+
 		Optional opcional = new Optional(1, "TC", "Techo corredizo", 12000.0);
-		Set<Integer> opcionales = new HashSet();
+		Set<Integer> opcionales = new HashSet<Integer>();
 		opcionales.add(opcional.getId());
 
-		CarDto car = new CarDto(model, opcionales);
+		CarDto car = new CarDto(modelId, opcionales);
+		assertEquals(modelId, car.getModel());
+		assertEquals(opcionales, car.getOptionals());
+	}
 
-		//assertEquals(, );
+	//@Test
+	public void actualizarUnAuto() {
+		CarRest cr = new CarRest();
+
+		Integer modelId = 3;
+		int id = 1;
+		Optional opcional = new Optional(3, "ABS", "Sistema de frenos ABS", 14000.0);
+		Set<Integer> opcionales = new HashSet<Integer>();
+		opcionales.add(opcional.getId());
+
+		CarDto car = new CarDto(modelId, opcionales);
+		cr.create(car);
+		cr.update(id, car);
+		
+	}
+
+	//@Test
+	public void calculateCost() {
+		CarRest cr = new CarRest();
+
+		Integer modelId = 1;
+		Optional opcional = new Optional(4, "LL", "Llantas de aleación", 12000.0);
+		Optional opcional2 = new Optional(1, "TC", "Techo corredizo", 12000.0);
+		Set<Integer> opcionales = new HashSet<Integer>();
+		opcionales.add(opcional.getId());
+		opcionales.add(opcional2.getId());
+
+		CarDto car = new CarDto(modelId, opcionales);
+		String resultado = cr.calculateCost(car);
+		String resultadoEsperado =  "254000.0";
+		assertEquals(resultadoEsperado, resultado);
+	}
+	
+	//@Test
+	public void metodoStats() {
+		CarRest cr = new CarRest();
+
 	}
 }
--- src/test/java/com/opendev/service/impl/OptionalServiceImplTest.java
+++ src/test/java/com/opendev/service/impl/OptionalServiceImplTest.java
@@ -28,33 +28,5 @@ public class OptionalServiceImplTest {
 		assertEquals(suma, resultado);
 	}
 	
-	//@Test
-	public void metodogetByIds() {
-		// assert equals 1, optional.getbyid
-		// preguntar como comparar getbyid (devuelve cada id del set de ids) 
-		
-		// setup
-		OptionalService os = new OptionalServiceImpl();
-		OptionalRepository op = new OptionalRepositoryImpl();
-		
-		Optional opcional1 = new Optional(1, "TC", "Techo corredizo", 12000.0);
-		Optional opcional2 = new Optional(2, "AA", "Aire acondicionado", 20000.0);
-		Optional opcional3 = new Optional(3, "ABS", "Sistema de frenos ABS", 14000.0);
-		
-		Set<Optional> opcionales = new HashSet<>();
-		opcionales.add(opcional1);
-		//opcionales.add(opcional2);
-		//opcionales.add(opcional3);
-
-		Set<Integer> idOptionals = new HashSet<>();
-		idOptionals.add(opcional1.getId());
-		//idOptionals.add(opcional2.getId());
-		//idOptionals.add(opcional3.getId());
-		
-		//Set<Optional> resultadoEsperado = os.getByIds(idOptionals);
-		assertEquals(os.getByIds(idOptionals), op.getOne(opcional1.getId()));
-
-	}
-	
 
 }
