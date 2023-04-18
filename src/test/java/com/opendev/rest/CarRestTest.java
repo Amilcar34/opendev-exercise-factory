6ef7ca415538cf44d832242495e0dc8ee417d971
avalos.m <mora_m_avalos@hotmail.com>
2023-04-18 12:20:47 -0300

    :D

--- src/test/java/com/opendev/rest/CarRestTest.java
+++ src/test/java/com/opendev/rest/CarRestTest.java
@@ -1,12 +1,12 @@
 package com.opendev.rest;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import java.util.Collections;
 import java.util.Set;
 
-import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import com.opendev.contracts.StatsCar;
@@ -16,24 +16,20 @@ import com.opendev.entity.Model;
 import com.opendev.entity.Optional;
 import com.opendev.service.impl.CarServiceImpl;
 
-import static org.mockito.Mockito.*;
-
 public class CarRestTest {
-	
+
 	CarServiceImpl carServiceMock = mock(CarServiceImpl.class);
 	CarRest carRest = new CarRest(carServiceMock);
 
 	@Test
-	@Order(1)
 	public void vUpdate() {
 
 		Model model = new Model(2, "Familiar", 270000.0);
 		Optional opcional1 = new Optional(3, "ABS", "Sistema de frenos ABS", 14000.0);
-		Optional opcional2 = new Optional(1, "TC", "Techo corredizo", 12000.0);
-		
-		Set<Integer> opcionalesId = Set.of(opcional1.getId(), opcional2.getId());
-		
-		Set<Optional> opcionales = Set.of(opcional2, opcional1);
+
+		Set<Integer> opcionalesId = Set.of(opcional1.getId());
+
+		Set<Optional> opcionales = Set.of(opcional1);
 
 		CarDto car = new CarDto(2, opcionalesId);
 		Car auto = new Car(1, model, opcionales);
@@ -41,10 +37,10 @@ public class CarRestTest {
 		when(carServiceMock.update(1, model.getId(), opcionalesId)).thenReturn(auto);
 
 		String resultado = carRest.update(1, car);
-		String jsonExpected = "{\"id\":1,\"model\":{\"id\":2,\"name\":\"Familiar\",\"cost\":270000.0},\"optionals\":[{\"id\":3,\"name\":\"ABS\",\"fullName\":\"Sistema de frenos ABS\",\"cost\":14000.0},{\"id\":1,\"name\":\"TC\",\"fullName\":\"Techo corredizo\",\"cost\":12000.0}],\"price\":296000.0}";
-	 
-		assertEquals(jsonExpected, resultado);
-	
+		String jsonExpected = "{\"id\":1,\"model\":{\"id\":2,\"name\":\"Familiar\",\"cost\":270000.0},\"optionals\":[{\"id\":3,\"name\":\"ABS\",\"fullName\":\"Sistema de frenos ABS\",\"cost\":14000.0}],\"price\":284000.0}";
+
+		Assertions.assertEquals(jsonExpected, resultado);
+
 	}
 
 	@Test
@@ -52,9 +48,9 @@ public class CarRestTest {
 
 		Optional opcional = new Optional(1, "TC", "Techo corredizo", 12000.0);
 		Model model = new Model(2, "Familiar", 270000.0);
-		
+
 		Set<Integer> opcionales = Set.of(opcional.getId());
-		
+
 		Set<Optional> setOPtionales = Collections.singleton(opcional);
 
 		CarDto car = new CarDto(2, opcionales);
@@ -64,8 +60,8 @@ public class CarRestTest {
 
 		String resultado = carRest.create(car);
 		String jsonExpected = "{\"id\":1,\"model\":{\"id\":2,\"name\":\"Familiar\",\"cost\":270000.0},\"optionals\":[{\"id\":1,\"name\":\"TC\",\"fullName\":\"Techo corredizo\",\"cost\":12000.0}],\"price\":282000.0}";
-		
-		assertEquals(jsonExpected, resultado);
+
+		Assertions.assertEquals(jsonExpected, resultado);
 	}
 
 	@Test
@@ -73,41 +69,41 @@ public class CarRestTest {
 		
 		when(carServiceMock.delete(1)).thenReturn(true);
 		
-		assertTrue(carRest.delete(1));
+		Assertions.assertTrue(carRest.delete(1));
 	}
 
 	@Test
 	public void calculateCost() {
-		
+
 		Optional opcional = new Optional(4, "LL", "Llantas de aleación", 12000.0);
 		Optional opcional2 = new Optional(1, "TC", "Techo corredizo", 12000.0);
-		
+
 		Set<Integer> opcionalesId = Set.of(opcional.getId(), opcional2.getId());
 
 		CarDto car = new CarDto(2, opcionalesId);
-		
+
 		when(carServiceMock.calculateCost(2, opcionalesId)).thenReturn(294000.0);
-		
+
 		String resultado = carRest.calculateCost(car);
 		String resultadoEsperado = "294000.0";
-		
-		assertEquals(resultadoEsperado, resultado);
+
+		Assertions.assertEquals(resultadoEsperado, resultado);
 	}
 
 	@Test
 	public void metodoStats() {
-		
+
 		StatsCar statsCar = new StatsCar();
 		statsCar.setCount_car(3);
 		statsCar.setCars(null);
 		statsCar.setOptionals(null);
-		
+
 		when(carServiceMock.stats()).thenReturn(statsCar);
-		
+
 		String resultado = carRest.stats();
 		String jsonExpected = "{\"count_car\":3}";
-		
-		assertEquals(resultado, jsonExpected);
-		
+
+		Assertions.assertEquals(resultado, jsonExpected);
+
 	}
 }
