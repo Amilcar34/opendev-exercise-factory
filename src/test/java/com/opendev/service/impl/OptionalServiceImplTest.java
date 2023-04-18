75823a476c5233e8d959f62b784697a63afbda8b
avalos.m <mora_m_avalos@hotmail.com>
2023-04-18 12:22:03 -0300

    revision

--- src/test/java/com/opendev/service/impl/OptionalServiceImplTest.java
+++ src/test/java/com/opendev/service/impl/OptionalServiceImplTest.java
@@ -1,33 +1,28 @@
 package com.opendev.service.impl;
 
-import static org.junit.Assert.assertEquals;
-
-import java.util.HashSet;
 import java.util.Set;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 import com.opendev.entity.Optional;
-import com.opendev.repository.OptionalRepository;
-import com.opendev.repository.impl.OptionalRepositoryImpl;
 import com.opendev.service.OptionalService;
-import com.opendev.service.impl.OptionalServiceImpl;
 
 public class OptionalServiceImplTest {
 
 	@Test
 	public void vSumCost() {
-		
+
 		OptionalService os = new OptionalServiceImpl();
 
 		Optional opcional1 = new Optional(1, "TC", "Techo corredizo", 12000.0);
 		Optional opcional2 = new Optional(2, "AA", "Aire acondicionado", 20000.0);
-		
+
 		Set<Optional> optionals = Set.of(opcional1, opcional2);
-		
+
 		Double suma = 32000.0;
 		Double resultado = os.sumCost(optionals);
-		assertEquals(suma, resultado);
+		Assertions.assertEquals(suma, resultado);
 	}
 
 }
