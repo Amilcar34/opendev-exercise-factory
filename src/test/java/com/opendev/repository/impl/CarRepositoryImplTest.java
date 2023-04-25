b6d845ff9bc111d4a973c6a6e108568f3e3c6dee
avalos.m <mora_m_avalos@hotmail.com>
2023-04-25 11:16:21 -0300

    borro @Disabled vgetOne

--- src/test/java/com/opendev/repository/impl/CarRepositoryImplTest.java
+++ src/test/java/com/opendev/repository/impl/CarRepositoryImplTest.java
@@ -1,4 +1,5 @@
 package com.opendev.repository.impl;
+
 import static com.opendev.repository.impl.ModelRepositoryImpl.dbModels;
 import static com.opendev.repository.impl.OptionalRepositoryImpl.dbOptionals;
 import static java.util.Set.of;
@@ -21,22 +22,21 @@ import com.opendev.repository.CarRepository;
 //@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class CarRepositoryImplTest {
 
-	 CarRepository carRepository = new CarRepositoryImpl();
+	CarRepository carRepository = new CarRepositoryImpl();
 
 	@Test
-	@Disabled
-		public void vGetOne() {
-			
-			Model model = new Model(1, "Sedán", 230000.0);
-			Optional optional1 = new Optional(4, "AB", "Airbag", 7000.0); 
-			Optional optional2 = new Optional(2, "AA", "Aire acondicionado", 20000.0); 
-			Set<Optional> optionals = Set.of(optional2, optional1);
-			
-			Car car = new Car(1, model, optionals);
-			
-			Assertions.assertEquals(car, carRepository.getOne(1));
-		}
-		
+	public void vGetOne() {
+
+		Model model = new Model(1, "Sedán", 230000.0);
+		Optional optional1 = new Optional(4, "AB", "Airbag", 7000.0);
+		Optional optional2 = new Optional(2, "AA", "Aire acondicionado", 20000.0);
+		Set<Optional> optionals = Set.of(optional2, optional1);
+
+		Car car = new Car(1, model, optionals);
+
+		Assertions.assertEquals(car, carRepository.getOne(1));
+	}
+
 	@Test
 	@Disabled
 	public void vSave() {
@@ -45,18 +45,18 @@ public class CarRepositoryImplTest {
 		Optional opcional1 = new Optional(1, "TC", "Techo corredizo", 12000.0);
 		Optional opcional2 = new Optional(2, "AA", "Aire acondicionado", 20000.0);
 		Optional opcional3 = new Optional(3, "ABS", "Sistemas de frenos ABS", 14000.0);
-		Optional opcional4 = new Optional(4, "AB", "Airbag", 7000.0); 
+		Optional opcional4 = new Optional(4, "AB", "Airbag", 7000.0);
 
 		Set<Optional> opcionales = Set.of(opcional1, opcional2, opcional3, opcional4);
 
 		Car auto = new Car(null, modelo, opcionales);
 
 		Car autoObtenido = carRepository.save(auto);
-		
+
 		Car autoEsperado = new Car(6, modelo, opcionales);
-		
+
 		Assertions.assertEquals(autoEsperado, autoObtenido);
-	
+
 	}
 
 	@Test
@@ -80,15 +80,15 @@ public class CarRepositoryImplTest {
 	@Test
 	public void vStatsModels() {
 		Set<StatsModel> expectedStatsModel = new HashSet<>();
-		
+
 		StatsModel modelo1 = new StatsModel("Sedán", 2, 40.0);
 		StatsModel modelo2 = new StatsModel("Coupé", 2, 40.0);
 		StatsModel modelo3 = new StatsModel("Familiar", 1, 20.0);
-		
+
 		expectedStatsModel.add(modelo1);
 		expectedStatsModel.add(modelo2);
 		expectedStatsModel.add(modelo3);
-		
+
 		Assertions.assertIterableEquals(expectedStatsModel, carRepository.statsModel());
 
 	}
@@ -100,30 +100,30 @@ public class CarRepositoryImplTest {
 		StatsOptional optional3 = new StatsOptional("TC", 2, 20.0);
 		StatsOptional optional4 = new StatsOptional("ABS", 1, 10.0);
 		StatsOptional optional5 = new StatsOptional("AB", 4, 40.0);
-		
+
 		statsOptionals.add(optional1);
 		statsOptionals.add(optional5);
 		statsOptionals.add(optional3);
 		statsOptionals.add(optional4);
-		
+
 		Assertions.assertEquals(statsOptionals, carRepository.statsOptional());
 
 	}
-	
+
 	@BeforeEach
 	public void setUp() {
-		
+
 		carRepository.deleteById(1);
 		carRepository.deleteById(2);
 		carRepository.deleteById(3);
 		carRepository.deleteById(4);
 		carRepository.deleteById(5);
 		carRepository.save(new Car(1, dbModels.get(1), of(dbOptionals.get(2), dbOptionals.get(4))));
-		carRepository.save( new Car(2, dbModels.get(1), of(dbOptionals.get(1), dbOptionals.get(4))));
-		carRepository.save( new Car(3, dbModels.get(2), of(dbOptionals.get(2), dbOptionals.get(4))));
-		carRepository.save( new Car(4, dbModels.get(3), of(dbOptionals.get(1), dbOptionals.get(2), dbOptionals.get(3), dbOptionals.get(4))));
-		carRepository.save( new Car(5, dbModels.get(3), of()));	
+		carRepository.save(new Car(2, dbModels.get(1), of(dbOptionals.get(1), dbOptionals.get(4))));
+		carRepository.save(new Car(3, dbModels.get(2), of(dbOptionals.get(2), dbOptionals.get(4))));
+		carRepository.save(new Car(4, dbModels.get(3),
+				of(dbOptionals.get(1), dbOptionals.get(2), dbOptionals.get(3), dbOptionals.get(4))));
+		carRepository.save(new Car(5, dbModels.get(3), of()));
 	}
 
 }
-
